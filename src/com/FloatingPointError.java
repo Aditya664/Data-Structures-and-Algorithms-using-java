37dcc5354ecf303021e90fe17a0447ae804f4232
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-05 08:46:12 -0700

    initial

---
+++ src/com/FloatingPointError.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.awt.desktop.ScreenSleepEvent;
+import java.util.Scanner;
+
+public class FloatingPointError {
+    public static void main(String[] args)
+    {
+// Values are not accurate in float
+        Scanner input = new Scanner(System.in);
+        float marks = input.nextFloat();
+        System.out.println(marks);
+        /*it gives this output
+         545454.5454545
+         545454.56*/
+    }
+}

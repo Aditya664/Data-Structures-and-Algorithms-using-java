08e66dbc8c31d4b39277712619dfb1fdd0fdbb61
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-05 08:51:27 -0700

    initial

---
+++ src/com/Sum.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        //Scan two numbers
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        //Get its sum
+        int sum = num1+num2;
+        //print sum
+        System.out.println("Sum of given no is:" + sum);
+    }
+}

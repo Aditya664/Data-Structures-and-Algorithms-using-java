46d52a53cb10d6abc153f3f5dcb7b0e759d1efc2
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-06 00:52:18 -0700

    initial

---
+++ src/com/NumberSystem/HowToWriteInJava.java
@@ -0,0 +1,20 @@
+package com.NumberSystem;
+
+public class HowToWriteInJava {
+    public static void main(String[] args)
+    {
+        //How to write number system in java?
+
+        int dec  = 152; //decimal fromat
+        int bin = 0b10011; //binary format
+        int oct = 0354; //octal format
+        int hex = 0x6565; //hexdeciamal format
+
+        System.out.println(dec);
+        System.out.println(bin);
+        System.out.println(oct);
+        System.out.println(hex);
+
+
+    }
+}
---
+++ src/com/NumberSystem/NumberSystem.java
@@ -0,0 +1,24 @@
+package com.NumberSystem;
+
+public class NumberSystem {
+    public static void main(String[] args)
+    {
+
+   /* 1.Binary number system-
+    Radix is 2
+    0,1
+    */
+
+  /*  2.Octal number system-
+    Radix is 8
+    0,1,2,3,4,5,6,7
+    */
+
+    /*  2.Hexadeciamal number system-
+    Radix is 16
+    0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+    */
+
+    }
+
+}

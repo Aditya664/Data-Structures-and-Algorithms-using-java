8b99e99cdc97c24e6c0e45ceecf5c110282ff597
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-10 20:32:34 -0700

    initial

--- src/com/BesicsOfJava/Array/Arrays.java
+++ src/com/BesicsOfJava/Array/Arrays.java
@@ -23,15 +23,23 @@ public class Arrays {
          System.out.println(arr3[0]);
          boolean[] arr4 = new boolean[10];
          System.out.println(arr4[0]);
-//         System.out.println(arr4[10]);
+         System.out.println(arr4[10]);
 
          //array litteral
-         int[] arr5 = {3,2,1};
-         int[][] matrix = {
-                 {1,2,3},
-                 {4,5,6}
-         };
-         System.out.println(matrix[0][2]);
+         int[] arr5 = {3, 2, 1};
 
+         int[][] matrix = {
+                 {1, 2, 3},
+                 {4, 5, 6}
+         };
+
+//		System.out.println(matrix[0][2]);
+
+         int[][] matrix2 = new int[10][10];
+//		System.out.println(matrix2[0]);
+
+       /*  System.out.println(java.util.Arrays.toString(arr5));
+         Arrays.sort(arr5);
+         System.out.println(Arrays.toString(arr5));*/
      }
 }

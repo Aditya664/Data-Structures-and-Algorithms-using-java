0bc9de9205654e21ac0a071eec0b9b2388bce084
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-10 20:22:32 -0700

    initial

---
+++ src/com/BesicsOfJava/Array/Arrays.java
@@ -0,0 +1,28 @@
+package com.BesicsOfJava.Array;
+
+public class Arrays {
+   /*  What is Array?
+    Array is represent a group of elements of same data type. We can represent elements in single array.
+        there are three types of array:
+            1.one dimensional array.
+            2.two ......
+            3.three ......*/
+     public static void main(String[] args)
+     {
+         int[] arr;
+         int arr2[];
+//         these are two ways to declare array
+
+         arr = new int[10];
+         //Initialize an array
+         System.out.print(arr.length);
+         //check length of an array
+         System.out.println(arr[0]);
+         //check the elements in array
+         double[] arr3 = new double[10];
+         System.out.println(arr3[0]);
+         boolean[] arr4 = new boolean[10];
+         System.out.println(arr4[0]);
+
+     }
+}

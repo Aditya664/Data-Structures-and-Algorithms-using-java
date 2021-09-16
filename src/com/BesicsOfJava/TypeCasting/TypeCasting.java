e5d4319d631d7bcb710d3b033a9ae4465dc7761a
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2021-09-16 23:28:44 +0600

    Added type casting

--- src/com/BesicsOfJava/TypeCasting/TypeCasting.java
+++ src/com/BesicsOfJava/TypeCasting/TypeCasting.java
@@ -41,6 +41,35 @@ public class TypeCasting {
             because its give  the reminder because max value is 257 hence byte
         show extra velue form the input for eq.if i give 260 it give me output 4.*/
 
+        byte c = 40;
+        byte d = 50;
+        byte e = 100;
+        int f = (c * d)/ e;
+        System.out.println(f);
+
+
+        int num4 = 'a';
+        System.out.println(num4);
+
+//        it gives askye value
+
+
+
+/*        Type promotion rules.
+            1.All the byte short and char values are promoted to int.
+            2.Any one of the operand is long whole operation promoted to long.*/
+
+        byte p = 42;
+        char q = 'a';
+        short w = 1024;
+        int r = 50000;
+        float s = 5.6f;
+        double t = 0.22223;
+        double result = (s * p) + (r / q) - (t * w);
+        //float + int - double = double.
+        System.out.println((s * p) + " " + (r / q) + " " +(t * w));
+        System.out.println(result);
+
 
 
     }

24bd06b1e77ab89430c361f15ee89d5fae6df751
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 07:11:53 -0700

    Exercise

--- src/com/BesicsOfJava/Loops/loops.java
+++ src/com/BesicsOfJava/Loops/loops.java
@@ -5,12 +5,7 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args)
     {
-
-        /*When you use which loop?
-            when you know how many times loops going to run then for loop is used.
-            and when you have no idea how many times loop going to run then while loop is used.*/
-//        ----------
-
+        Whn         
         //for loop
         /*syntax of for loop:
         for(initialisaion;condition;increament/decrement)
@@ -18,13 +13,11 @@ public class loops {
             {
                 //body
             }*/
-        System.out.println("-----For loop-----");
         for(int num = 1;num <= 5; num += 1)
         {
             System.out.println(num);
         }
 
-        System.out.println("-----while loop-----");
         //While loop
      /*   syntax for while loop:
         while(confition)
@@ -37,25 +30,6 @@ public class loops {
             System.out.println(num);
             num++;
         }
-
-        System.out.println("-----do-while loop-----");
-        //do-while loop
-       /*
-        syntax of do while loop:
-        do {
-
-        }while(condition);*/
-
-
-       /* why we use do while loop?
-            in the do while loop the loop going to executes atleast one.
-            */
-        int n = 1;
-        do{
-            System.out.println(n);
-            n++;
-        }while(n <= 5);
-
 
     }
  }
---
+++ src/com/BesicsOfJava/Switch/Switch.java
@@ -0,0 +1,4 @@
+package com.BesicsOfJava.Switch;
+
+public class Switch {
+}

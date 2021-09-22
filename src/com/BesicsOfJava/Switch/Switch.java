9d81c82d6542df08bfbb8dd7335cb30bf1c1681e
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 08:07:46 -0700

    Enhance Swich

--- src/com/BesicsOfJava/Switch/Switch.java
+++ src/com/BesicsOfJava/Switch/Switch.java
@@ -7,22 +7,12 @@ public class Switch {
         Scanner in = new Scanner(System.in);
         String fruit = in.next();
 
-        switch(fruit){
-            case "Mango":
-            System.out.println("King of fruit");
-            break;
-            case "Apple":
-                System.out.println("A Sweet red fruit");
-                break;
-            case "Orange":
-                System.out.println("Round fruit");
-                break;
-            case "Grapes":
-                System.out.println("Small fruit");
-                break;
-            default:
-                System.out.println("Please enter valid fruit");
-
-    }
+        switch (fruit) {
+            case "Mango" -> System.out.println("King of fruit");
+            case "Apple" -> System.out.println("A Sweet red fruit");
+            case "Orange" -> System.out.println("Round fruit");
+            case "Grapes" -> System.out.println("Small fruit");
+            default -> System.out.println("Please enter valid fruit");
+        }
     }
 }

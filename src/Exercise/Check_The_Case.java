3d992aac123777b8d97c89ceb80585963add2427
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 05:26:59 -0700

    Exercise

---
+++ src/Exercise/Check_The_Case.java
@@ -0,0 +1,16 @@
+package Exercise;
+
+import java.util.Scanner;
+
+public class Check_The_Case {
+    public static void main(String[] args)
+    {
+        //Q.Check the type of character.
+        Scanner in = new Scanner(System.in);
+        /*What is trim and charAt?
+            trim means that are remove all extra spaces that are the end of the word.
+            charAt means give me the index of the string its array type*/
+        char ch = in.next().trim().charAt(0);
+
+    }
+}

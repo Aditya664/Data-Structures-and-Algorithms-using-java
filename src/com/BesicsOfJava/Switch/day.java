811e44ee9f0ea038cc148e3d3ade708e3927aeaf
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 08:51:10 -0700

    Added day of the week

--- src/com/BesicsOfJava/Switch/day.java
+++ src/com/BesicsOfJava/Switch/day.java
@@ -1,7 +1,20 @@
 package com.BesicsOfJava.Switch;
 
+import java.util.Scanner;
+
 public class day {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        int day = in.nextInt();
+        switch (day) {
+            case 1 -> System.out.println("monday");
+            case 2 -> System.out.println("tuesday");
+            case 3 -> System.out.println("wednesday");
+            case 4 -> System.out.println("thursday");
+            case 5 -> System.out.println("friday");
+            case 6 -> System.out.println("saturday");
+            case 7 -> System.out.println("sunday");
+            default -> System.out.println("Please enter valid input");
+        }
     }
 }

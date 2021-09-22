84635bbc59358006a4e34949f4a096e8ec7df7f3
Aditya <adityadeshmukh7350@gmail.com>
2021-09-22 07:12:32 -0700

    loops

--- src/com/BesicsOfJava/Loops/loops.java
+++ src/com/BesicsOfJava/Loops/loops.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args)
     {
-        Whn         
+
         //for loop
         /*syntax of for loop:
         for(initialisaion;condition;increament/decrement)

51f636309c40d8a890e7a64ea356ade5b8fb76a1
Aditya <adityadeshmukh7350@gmail.com>
2021-09-26 02:52:51 -0700

    Added function files

---
+++ src/com/BesicsOfJava/Function/Greatting.java
@@ -0,0 +1,10 @@
+package com.BesicsOfJava.Function;
+
+public class Greatting {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting(){
+        System.out.println("Hello");
+    }
+}

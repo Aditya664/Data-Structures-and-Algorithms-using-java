e04873acc2bda501364ea95b8dcc778a3e422a19
Aditya <adityadeshmukh7350@gmail.com>
2021-09-26 05:57:45 -0700

    Added Swap exercise

---
+++ src/Exercise/Swap.java
@@ -0,0 +1,13 @@
+package Exercise;
+
+public class Swap {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+    }
+}

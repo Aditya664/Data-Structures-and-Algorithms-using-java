39f2517df99d5c4912905c5f59720909c14b798e
Aditya <Adityadeshmukh7350@gmail.com>
2021-10-02 09:20:37 -0700

    Added function

--- src/Exercise/PrintArmstrong.java
+++ src/Exercise/PrintArmstrong.java
@@ -6,9 +6,13 @@ public class PrintArmstrong {
     public static void main(String[] args) {
         //print all the 3 digits armstrong number
         Scanner in = new Scanner(System.in);
-        System.out.println("Please enter 3 digit number: ");
-        int n = in.nextInt();
-        System.out.println(isArm(n));
+//        System.out.println("Please enter 3 digit number: ");
+//        int n = in.nextInt();
+      for(int i = 100; i < 1000; i++){
+          if(isArm(i)){
+              System.out.println(i);
+          }
+      }
 
 
 

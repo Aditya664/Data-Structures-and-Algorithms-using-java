f1058609acb9490339eb38b0cede232bec0dd2a3
Aditya <Adityadeshmukh7350@gmail.com>
2021-10-17 07:04:01 -0700

    Added New String

---
+++ src/com/BesicsOfJava/String/StringFormattingDemo.java
@@ -0,0 +1,20 @@
+package com.BesicsOfJava.String;
+public class StringFormattingDemo {
+
+    public static void main(String[] args) {
+        System.out.println("===== String formatting =====");
+        System.out.println();
+        String firstName = "Adi";
+
+        String greetingsTemplate = "Hello, dear %s! Good %s!";
+
+        String morning = "morning";
+        String afternoon = "afternoon";
+        String evening = "evening";
+        String formattedString = String.format(greetingsTemplate, firstName, morning);
+        System.out.println(formattedString);
+
+        System.out.printf("You have %d computers available at store\n", 10);
+    }
+
+}
